7df04ac6745c40c62bb5e40f6d3c9e69af9de677
al4nomg <alancasalnikonowicz@gmail.com>
2026-04-09 10:24:45 -0300

    Add files via upload

---
+++ prueba-01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+//Salto de línea
+    printf("Uno\tDos\tTres\n\n");
+
+//Salto de tabulación
+    printf("Uno\nDos\nTres\n\n");
+
+//Comilla doble literal
+    printf("Hola \"mundo\" de C\n\n");
+
+//Comilla simple literal
+    printf("Hola \'mundo\' de C\n\n");
+
+//Barra invertida literal 
+    printf("El archivo esta en D:\\");
+    
+    return 0;
+
+}
